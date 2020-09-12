415b9968e6caf835ed0466e0f574996055c2d7a9
Masahiro Nagano <kazeburo@gmail.com>
2020-09-12 15:49:47 +0900

    cnt

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -519,8 +519,8 @@ func searchChairs(c echo.Context) error {
 	}()
 
 	chairs := []Chair{}
-	params = append(params, perPage, page*perPage)
-	err = db.Select(&chairs, searchQuery+searchCondition+limitOffset, params...)
+	paramsQ := append(params, perPage, page*perPage)
+	err = db.Select(&chairs, searchQuery+searchCondition+limitOffset, paramsQ...)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return c.JSON(http.StatusOK, ChairSearchResponse{Count: 0, Chairs: []Chair{}})
@@ -796,8 +796,8 @@ func searchEstates(c echo.Context) error {
 	}()
 
 	estates := []Estate{}
-	params = append(params, perPage, page*perPage)
-	err = db.Select(&estates, searchQuery+searchCondition+limitOffset, params...)
+	paramsQ := append(params, perPage, page*perPage)
+	err = db.Select(&estates, searchQuery+searchCondition+limitOffset, paramsQ...)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return c.JSON(http.StatusOK, EstateSearchResponse{Count: 0, Estates: []Estate{}})
